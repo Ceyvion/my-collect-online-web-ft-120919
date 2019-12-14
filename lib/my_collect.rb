778e44b722eef74ace361311587c01c1924a0613
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-14 19:47:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,17 +6,19 @@
 
 
 
-def my_collect(i)
+def my_collect(array)
   new_array = []
   counter = 0 
-  while counter < self.length 
-  new_array << yield 
+  while counter < array.length 
+  new_array << yield array[counter]
   counter += 1 
   end #while
 end 
 my_collect(i)
 
 
-my_collect(i) do |upcase| puts i.upcase end 
+my_collect(array) do 
+  |upcase| puts i.upcase 
+end 
 
 
