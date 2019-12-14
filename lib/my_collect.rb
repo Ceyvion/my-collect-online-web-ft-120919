369d804873de43f656a5619a6897490db2b48cd3
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-14 20:30:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,27 @@
+#build a method >
+#take a an argument of an array >
+#use a counter to iterate using a while loop >
+#use a yield to go through each element 
+#store the results in a new collection array 
+
+
+
+def my_collect(array)
+  index = 0 
+  new_array = []
+  
+  
+  while index < array.length
+    new_array << yield(array[index])
+    index += 1 
+  end #while
+  
+  
+  new_array
+end 
+
+# my_collect(array) do 
+#   |upcase| i.upcase 
+# end 
 
 
